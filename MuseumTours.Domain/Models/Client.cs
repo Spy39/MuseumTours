afce8ea0610ea7e18e930b95436712bb7524fc02
Aleks <67071274+Spy39@users.noreply.github.com>
2023-08-10 16:54:19 +0300

    Add files via upload

---
+++ MuseumTours.Domain/Models/Client.cs
@@ -0,0 +1,13 @@
+﻿namespace MuseumTours.Domain.Models
+{
+	public class Client : Entity
+	{
+		public string LastName { get; set; }
+		public string FirstName { get; set; }
+		public string FatherName { get; set; }
+		public string Organization { get; set; }
+		public string Telephone { get; set; }
+		public string Mail { get; set; }
+		public string AdditionalInformation { get; set; }
+	}
+}
---
+++ MuseumTours.Domain/Models/Excursion.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MuseumTours.Domain.Models
+{
+	public class Excursion : Entity
+	{
+		public DateTime DateTime { get; set; }
+		public string TourName { get; set; }
+	}
+}
